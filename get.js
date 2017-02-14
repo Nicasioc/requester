ceb8719d7502f72a59534bf8edb4a97d00082c4a
Nicasioc <nicasioc@gmail.com>
2017-02-14 14:33:33 -0300

    get

---
+++ get.js
@@ -0,0 +1,33 @@
+var Promise = require("promise")
+
+module.export = function (url) {
+  
+  var promise = new Promise(function (accept, cancel) {
+
+    var ajax = new XMLHttpRequest();
+
+    promise.cancelRequest = function () {
+      ajax.abort();
+      reject(new Error("request cancel"));
+    };
+    
+    ajax.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status >= 200 && this.status < 300) {
+        accept(this);
+      } else {
+        reject(this);
+      }
+    };
+
+    ajax.open("GET", url, true);
+    ajax.send();
+
+  });
+  
+  
+  return promise;
+};
+/*
+get("url").then(()=>{})
+get("url").cancel();
+*/
---
+++ index.js
@@ -0,0 +1,35 @@
+function get(url) {
+  
+  var promise = new Promise(function (accept, cancel) {
+
+    var ajax = new XMLHttpRequest();
+
+    promise.cancelRequest = function () {
+      ajax.abort();
+      reject(new Error("request cancel"));
+    };
+    
+    ajax.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status >= 200 && this.status < 300) {
+        accept(this);
+      } else {
+        reject(this);
+      }
+    };
+
+    ajax.open("GET", url, true);
+    ajax.send();
+
+  });
+  
+  
+  return promise;
+};
+
+get("http://google.com").then(function(r) {console.log(r);})
+
+/*
+module.exports = {
+  get: require("./get")
+}
+*/
